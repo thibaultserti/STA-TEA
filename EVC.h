84393f0fc57a577b9b28a14d034ab3e9ac896653
Thibault Serti <thibault.serti@gmail.com>
2019-10-09 17:32:28 +0200

    Add prototypes of EVC and RBC

---
+++ EVC.h
@@ -0,0 +1,10 @@
+#ifndef DEF_EVC
+#define DEF_EVC
+
+bool is_moving = false;
+short unsigned local;
+
+bool send_local();
+
+
+ #endif
---
+++ RBC.h
@@ -0,0 +1,35 @@
+#ifndef DEF_RBC
+#define DEF_RBC
+
+#define MAX_LENG_ID 6
+#define MAX_NB_TRAINS 100
+
+typedef int bool;
+#define true 1
+#define false 0
+
+typedef struct train
+{
+    char* id[MAX_LENG_ID];
+    short signed local;
+    short signed eoa;
+
+} 
+Train;
+
+typedef struct trains
+{
+    Train* trains[MAX_NB_TRAINS];
+}
+Trains;
+
+//bool init_connection();
+bool add_from_rbc(Train *t);
+bool remove_from_rbc(Train *t);
+bool update_local_rbc(char* id, short local);
+bool check_eoa(void); /* send SIG_STOP if the train 2 is right behind the train 1; send SIG_CONTINUE otherwise */
+bool update_eoa_rbc(void);
+
+
+
+ #endif
