cb9f92521f3c948a0c16c460eea7fdaba29f1ec8
haran <ara.rahier@gmail.com>
2019-11-04 17:16:48 +0100

    :art: Add headers for EVC

--- EVC.c
+++ EVC.c
@@ -6,58 +6,6 @@
 #include<string.h>
 #include "EVC.h"
 
-int SocketCreate(void)
-{
-	int new_socket;
-	//Create socket
-	new_socket = socket(AF_INET, SOCK_STREAM, 0);
-	return new_socket;
-}
-
-void SocketConnect(int socket_desc, char* adresse_hote)
-{
-	int return_value = -1;
-	struct sockaddr_in server;
-	server.sin_addr.s_addr = inet_addr(adresse_hote);
-	server.sin_family = AF_INET;
-	server.sin_port = htons(PORT_NUMBER);
-	//Vérification de la connection
-	while (true)
-	{
-	    return_value = connect(socket_desc, (struct sockaddr *)&server, sizeof(server));
-	    perror("Connection\n");
-	    if(return_value<0)
-        {
-            printf("Could not connect to RBC\n");
-        }
-	    if(return_value == 0)
-        {
-            break;
-
-        }
-	}
-}
-
-int SocketReceive(int socket, char* response, short rcvSize)
-{
-    int rcv = -1;
-    while(rcv<0)
-    {
-        rcv = read(socket, response, rcvSize-1);
-        if(rcv < 0) /*Vérification nécessaire mais pas élégante. L'idéal serait de pouvoir exécuter une instruction
-            qu'après la première instance du while */
-        {
-            perror("Reading stream message");
-        }
-        if (rcv == 0)
-        {
-            perror("Ending connection in receive\n");
-            return 0;
-        }
-    }
-    return 1;
-}
-
 int main(int argc , char *argv[])
 {
     id = argv[2];
@@ -91,7 +39,7 @@ int main(int argc , char *argv[])
         if (send(socket_desc, data, strlen(data), 0)<0)
             perror("Writing stream message");
         else{
-            printf("Sending the following request : %s\n",data);
+            printf("Sending the following message : %s\n",data);
         }
 
         char response[SIZEOF_MSG] = "";
@@ -118,4 +66,56 @@ int main(int argc , char *argv[])
         }
 	} while(true);
 	return 1;
+}
+
+int SocketCreate(void)
+{
+    int new_socket;
+    //Create socket
+    new_socket = socket(AF_INET, SOCK_STREAM, 0);
+    return new_socket;
+}
+
+void SocketConnect(int socket_desc, char* adresse_hote)
+{
+    int return_value = -1;
+    struct sockaddr_in server;
+    server.sin_addr.s_addr = inet_addr(adresse_hote);
+    server.sin_family = AF_INET;
+    server.sin_port = htons(PORT_NUMBER);
+    //Vérification de la connection
+    while (true)
+    {
+        return_value = connect(socket_desc, (struct sockaddr *)&server, sizeof(server));
+        perror("Connection\n");
+        if(return_value<0)
+        {
+            printf("Could not connect to RBC\n");
+        }
+        if(return_value == 0)
+        {
+            break;
+
+        }
+    }
+}
+
+int SocketReceive(int socket, char* response, short rcvSize)
+{
+    int rcv = -1;
+    while(rcv<0)
+    {
+        rcv = read(socket, response, rcvSize-1);
+        if(rcv < 0) /*Vérification nécessaire mais pas élégante. L'idéal serait de pouvoir exécuter une instruction
+            qu'après la première instance du while */
+        {
+            perror("Reading stream message");
+        }
+        if (rcv == 0)
+        {
+            perror("Ending connection in receive\n");
+            return 0;
+        }
+    }
+    return 1;
 }
--- EVC.h
+++ EVC.h
@@ -8,4 +8,8 @@ bool is_moving = false;
 char* id;
 char* localisation;
 
+int SocketCreate(void);
+void SocketConnect(int socket_desc, char* adresse_hote);
+int SocketReceive(int socket, char* response, short rcvSize);
+
  #endif
